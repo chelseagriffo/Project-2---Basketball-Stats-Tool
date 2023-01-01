f44ae4069db132e3cfd120508d5a3471c13be7af
Chelsea Griffo <chelseagriffo@gmail.com>
2023-01-01 17:08:44 -0600

    Added 'name', 'guardians', and converted 'experience' to a bool

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ def clean_data():
     cleaned_players = []
     for player_stats in PLAYERS:
         clean_stats = {
+            'name':player_stats['name']
+            'guardians':player_stats['guardians']
+            if player_stats['experience'] == "YES"
+                clean_stats['experience'] = True
+            else:
+                clean_stats['experience'] = False
             
         }
 
