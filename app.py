45cd8dee24549fd1771eceaa37fed3ef172bccb3
Chelsea Griffo <chelseagriffo@gmail.com>
2023-01-01 18:53:53 -0600

    Corrected clean_data function
    Added commas to clean_stats dictionary, moved if-else statement and split/int method outside of the for loop, and added a return statement to the end of the function

--- app.py
+++ app.py
@@ -4,22 +4,32 @@ from constants import TEAMS, PLAYERS
 def clean_data():
     cleaned_players = []
     for player_stats in PLAYERS:
+        if player_stats['experience'] == "YES":
+            experience_bool = True
+        else:
+            experience_bool = False
+        height_int = int(player_stats['height'].split(' ')[0])
         clean_stats = {
-            'name':player_stats['name']
-            'guardians':player_stats['guardians']
-            if player_stats['experience'] == "YES"
-                clean_stats['experience'] = True
-            else:
-                clean_stats['experience'] = False
-            clean_stats['height_num'] = int(player_stats['height'].split(' ')[0])
+            'name':player_stats['name'],
+            'guardians':player_stats['guardians'],
+            'experience':experience_bool,
+            'height':height_int
             }
         cleaned_players.append(clean_stats)
+    return cleaned_players
 
 
 def balance_teams():
-    pass
+    panthers_roster = []
+    bandits_roster = []
+    warriors_roster = []
+    num_players_roster = len(PLAYERS) / len(TEAMS)
+    for team in TEAMS:
+        for clean_stats in cleaned_players: 
+            roster.append(clean_stats['name'])
 
 
 if __name__ == "__main__":
-    clean_data()
-
+    # print(clean_data())
+    # print(PLAYERS)
+    pass
